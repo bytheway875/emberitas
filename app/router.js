cf102cf3798e68c1fa5caf05011226a1d2f6437b
Shannon <s.byrne2254@gmail.com>
2016-07-10 18:16:13 -0500

    Add the schedule

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('schedule')
   this.route('resources', function(){
     this.route('installation', function(){
       this.route('mac');
